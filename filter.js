ebb41fd03ec7c211aeea5eec2a222f4f3583d453
Elizabeth Kritskaya <post.kri@gmail.com>
2024-06-16 17:30:58 +0300

    filter

---
+++ filter.js
@@ -0,0 +1,7 @@
+Arr = [ [ "foo", "foo@foo.com" ], [ "bar_", "bar@bar.com" ] ]
+
+function searchNames( logins ){
+   return  logins.filter((login) => {return login[0].slice(-1) === '_'})
+}
+
+console.log(searchNames(Arr))
